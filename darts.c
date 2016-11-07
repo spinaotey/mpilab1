cebffc4421405afeb0943980141687a397011b85
Sebastian Pina Otey <spinaotey@gmail.com>
2016-11-07 11:42:35 +0100

    Comment main function

--- darts.c
+++ darts.c
@@ -8,31 +8,53 @@
 #define ndarts     50000
 #define rounds     100
 
+/*  Dboard
+ * 
+ *  Computes an approximation of pi by throwing darts to a board and
+ *  looking how many are inside the circle of radius 1.
+ *
+ *  Variables:
+ *      -darts = Number of darts to throw for the approximaton
+ */
 
 double dboard(int darts);
 
+
+/*  Computes an approximation of pi by throwing ndarts in parallel
+ *  to different boards, and averages the resulint pies. This is
+ *  done for many rounds.
+ */
+
 int main(int argc, char *argv[]){
-    int i;
-    int err;
-    int rank, size;
-    double pi,result;
+    /* VARIABLES */
+    int i;              // Dummy variable
+    int err;            // Error handling
+    int rank, size;     // MPI variables
+    double pi,result;   // Pi approximations and final result
     
+    /* INTIATE PARALLELIZATION AND RANDOM */
     err = MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
-    srandom(((int) time(NULL)) +rank);
+    srandom(((int) time(NULL)) +rank); // A different random seed for each process
+
+
+    /* MAIN PROGRAM */
 
     for(i=0; i<rounds; i++){
         pi = dboard(ndarts);
+        // Sum over all the pi's obtained and send it to the master
         MPI_Reduce(&pi, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        // Master computes average and prints it out
         if(rank == 0){
             result /= size;
             printf("π = %g\n",result);
         }
     }
 
+    /* FINISH PARALLELIZATION */
     err = MPI_Finalize();
 
     return 0;
